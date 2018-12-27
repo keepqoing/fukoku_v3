16ccb77b39684c8cb63a7f5050f9ac8873965513
phengtola <tolapheng99@gmail.com>
2018-12-27 06:11:09 -0800

    Initial commit

---
+++ src/main/java/kr/co/fukoku/FukokuV3Application.java
@@ -0,0 +1,14 @@
+package kr.co.fukoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FukokuV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FukokuV3Application.class, args);
+	}
+
+}
+
